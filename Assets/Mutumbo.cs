a23bf873755e60ac2e45f88788156381d7812b05
Mamadou <m.hussein@saedubai.com>
2022-08-23 00:29:39 +0400

    AStar API completed

---
+++ Assets/Mutumbo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AStarAPI;
+public class Mutumbo : MonoBehaviour
+{
+    
+    private void Start()
+    {
+       // GridAStar grid = new GridAStar();
+       // grid.CreateGrid();
+    }
+}
